208126c221134876e8c907f72c777a4547669608
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-25 12:55:31 +0930

    Removed server var by accident

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get('*', function(req, res) {
     res.render('http/404');
 });
 
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
