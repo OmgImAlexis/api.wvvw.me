34ad2fcce22db191fb175b792ad2e785b4994741
Alexis Tyler <OmgImAlexis@gmail.com>
2014-09-25 12:49:44 +0930

    Added logging

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var subdomain = require('subdomain')
 var app = express();
+var morgan = require('morgan');
+var fs = require('fs');
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {
+    flags: 'a'
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -11,6 +16,10 @@ app.use(subdomain({
 }));
 
 app.use('/subdomain/assets/', express.static(__dirname + '/public'));
+
+app.use(morgan('combined', {
+    stream: accessLogStream
+}))
 
 app.get('/', function(req, res) {
     res.render('index');
