338455ec4d4acabe91a2a86129f9b80e32452afb
Alexis Tyler <xo@wvvw.me>
2015-10-15 20:04:14 +1030

    Added bower support

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        bower_concat: {
+            all: {
+                dest: './_bower.js',
+                cssDest: './_bower.css',
+                exclude: [
+                    // 'jquery',
+                    // 'modernizr'
+                ],
+                dependencies: {
+                    // 'underscore': 'jquery',
+                    // 'backbone': 'underscore',
+                    // 'jquery-mousewheel': 'jquery'
+                },
+                bowerOptions: {
+                    relative: false
+                },
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    './public/js/_bower.min.js': ['./_bower.js'],
+                    // './public/js/_bower.min.css': ['./_bower.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['bower_concat', 'uglify']);
+
+};
