40d1913d83721b3a43a993b57f071f7268889f88
Alexis Tyler <xo@wvvw.me>
2017-04-02 16:08:55 +0930

    more cleanup

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM mhart/alpine-node:latest
+MAINTAINER Alexis Tyler <xo@wvvw.me>
+
+RUN npm install --global yarn
+
+RUN apk --no-cache add tini git openssh-client \
+    && apk --no-cache add --virtual devs tar curl
+
+RUN mkdir /app
+
+WORKDIR /app
+ADD . .
+
+RUN yarn install
+
+ADD nginx.conf.sigil .
+
+ENV MONGO_URL 'localhost:27017'
+ENV MONGO_USER ''
+ENV MONGO_PASS ''
+
+EXPOSE 5000
+
+ENTRYPOINT ["/sbin/tini"]
+
+CMD ["yarn", "start"]
--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
-const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR || 12;
 
 const userSchema = new Schema({
     displayName: {
@@ -26,23 +25,14 @@ userSchema.pre('save', function(next) {
     // only hash the password if it has been modified (or is new)
     if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+    bcrypt.hash(user.password, null, null, (err, hash) => {
+        user.password = hash;
+        next();
     });
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+userSchema.methods.comparePassword = (candidatePassword, cb) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
         if (err) return cb(err);
         cb(null, isMatch);
     });
